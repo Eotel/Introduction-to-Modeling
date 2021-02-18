4ad48f5b68a9dc4472e77e369e83fd1071d29d85
miura daiki <49769982+Eotel@users.noreply.github.com>
2021-02-19 03:06:17 +0900

    Add files via upload

--- saizen/app/Tracer.cpp
+++ saizen/app/Tracer.cpp
@@ -20,24 +20,6 @@ void Tracer::terminate() {
   rightWheel.stop();
 }
 
-float Tracer::calc_prop_value() {
-  const float Kp = 0.83;
-  const int target = 10;
-  const int bias = 0;
-
-  int diff = colorSensor.getBrightness() - target;
-  return (Kp * diff + bias);
-}
-
-void Tracer::run() {
-  msg_f("running...", 1);
-  float turn = calc_prop_value();
-  int pwm_l = pwm - turn;
-  int pwm_r = pwm + turn;
-  leftWheel.setPWM(pwm_l);
-  rightWheel.setPWM(pwm_r);
-}
-
 void Tracer::manual_run(int leftPWM, int rightPWM) {
   msg_f("rounding...", 1);
   leftWheel.setPWM(leftPWM);
--- saizen/app/Tracer.h
+++ saizen/app/Tracer.h
@@ -1,5 +1,4 @@
 #include "Motor.h"
-#include "ColorSensor.h"
 #include "util.h"
 
 using namespace ev3api;
@@ -7,7 +6,6 @@ using namespace ev3api;
 class Tracer {
 public:
   Tracer();
-  void run();
   void init();
   void terminate();
   void manual_run(int leftPWM, int rightPWM);
@@ -16,9 +14,4 @@ public:
 private:
   Motor leftWheel;
   Motor rightWheel;
-  ColorSensor colorSensor;
-  const int8_t mThreshold = 20;
-  const int8_t pwm = (Motor::PWM_MAX) / 6;
-
-  float calc_prop_value();
 };
